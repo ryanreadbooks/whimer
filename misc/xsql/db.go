d3fdc05e5432dbd0500f2f567b8c4732319d3530
ryanreadbooks <ryanreadbooks@foxmail.com>
2024-11-08 20:22:54 +0800

    feat(misc): wrap sqlx.SqlConn

---
+++ misc/xsql/db.go
@@ -0,0 +1,93 @@
+package xsql
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+type TransactFunc func(ctx context.Context, s sqlx.Session) error
+
+type beginTxKey struct{}
+
+// 对db的封装
+type DB struct {
+	sc sqlx.SqlConn
+}
+
+func New(s sqlx.SqlConn) *DB {
+	d := &DB{
+		sc: s,
+	}
+
+	return d
+}
+
+func (d *DB) getSess(ctx context.Context) sqlx.Session {
+	if tx, ok := ctx.Value(beginTxKey{}).(sqlx.Session); ok {
+		return tx
+	}
+	return d.sc
+}
+
+func (d *DB) Conn() sqlx.SqlConn {
+	return d.sc
+}
+
+func (d *DB) Transact(ctx context.Context, fn func(ctx context.Context) error) error {
+	err := d.sc.TransactCtx(ctx, func(ctx context.Context, s sqlx.Session) error {
+		ctx = context.WithValue(ctx, beginTxKey{}, s)
+		return fn(ctx)
+	})
+
+	return err
+}
+
+func (d *DB) Exec(query string, args ...any) (sql.Result, error) {
+	return d.sc.Exec(query, args...)
+}
+
+func (d *DB) ExecCtx(ctx context.Context, query string, args ...any) (sql.Result, error) {
+	return d.getSess(ctx).ExecCtx(ctx, query, args...)
+}
+
+func (d *DB) Prepare(query string) (sqlx.StmtSession, error) {
+	return d.sc.Prepare(query)
+}
+
+func (d *DB) PrepareCtx(ctx context.Context, query string) (sqlx.StmtSession, error) {
+	return d.getSess(ctx).PrepareCtx(ctx, query)
+}
+
+func (d *DB) QueryRow(v any, query string, args ...any) error {
+	return d.sc.QueryRow(v, query, args...)
+}
+
+func (d *DB) QueryRowCtx(ctx context.Context, v any, query string, args ...any) error {
+	return d.getSess(ctx).QueryRowCtx(ctx, v, query, args...)
+}
+
+func (d *DB) QueryRowPartial(v any, query string, args ...any) error {
+	return d.sc.QueryRowPartial(v, query, args...)
+}
+
+func (d *DB) QueryRowPartialCtx(ctx context.Context, v any, query string, args ...any) error {
+	return d.getSess(ctx).QueryRowPartialCtx(ctx, v, query, args...)
+}
+
+func (d *DB) QueryRows(v any, query string, args ...any) error {
+	return d.sc.QueryRows(v, query, args...)
+}
+
+func (d *DB) QueryRowsCtx(ctx context.Context, v any, query string, args ...any) error {
+	return d.sc.QueryRowsCtx(ctx, v, query, args...)
+}
+
+func (d *DB) QueryRowsPartial(v any, query string, args ...any) error {
+	return d.sc.QueryRowsPartial(v, query, args...)
+}
+
+func (d *DB) QueryRowsPartialCtx(ctx context.Context, v any, query string, args ...any) error {
+	return d.getSess(ctx).QueryRowsPartialCtx(ctx, v, query, args...)
+}
